b4c0ef26d53905ae338416b474baf77f154a4ca2
timer1212 <neetessentials720@gmail.com>
2025-03-10 15:21:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+let timer;
 function startCountdown() {
-    let seconds = 60;  
-    let timer = setInterval(() => {
-        document.getElementById("timer").innerText = seconds;
-        if (seconds === 0) clearInterval(timer);
-        seconds--;
+    let minutes = document.getElementById("minutes").value || 1; // Default to 1 min
+    let seconds = minutes * 60;
+
+    clearInterval(timer); // Clear previous timers
+
+    timer = setInterval(() => {
+        let min = Math.floor(seconds / 60);
+        let sec = seconds % 60;
+
+        // Format time as MM:SS
+        document.getElementById("timer").innerText = `${min}:${sec < 10 ? '0' : ''}${sec}`;
+
+        if (seconds === 0) {
+            clearInterval(timer);
+        } else {
+            seconds--;
+        }
     }, 1000);
 }
+
